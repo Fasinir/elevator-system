8e481c8a03f5f2a10ff630ab27463c8b27fa1ee7
Fasinir <filipson17@gmail.com>
2022-03-26 17:38:50 +0100

    specified main class for jar, added testng dependency

--- src/main/java/com/filippiwosz/elevatorsystem/Main.java
+++ src/main/java/com/filippiwosz/elevatorsystem/Main.java
@@ -1,5 +1,10 @@
-package com.filippiwosz.elevatorsystem;/**
+package com.filippiwosz.elevatorsystem;
+
+/**
  * @author Filip Piwosz
-*/
-class Main {
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
 }
